23ad920022fe07857fbc8c3d6e97630e8560df23
Gabor Vizi <fasten90@gmail.com>
2019-01-26 17:17:49 +0100

    Added header test example

--- PdfGenerator.py
+++ PdfGenerator.py
@@ -1,7 +1,11 @@
 import os
 
+
 from reportlab.lib.pagesizes import A4
-from reportlab.pdfgen import canvas
+from reportlab.lib.units import cm
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import BaseDocTemplate, Frame, PageTemplate, Paragraph
+from functools import partial
 
 
 ###############################################################################
@@ -12,15 +16,39 @@ def debug_log(str):
 
 
 def make_directory(directory):
-
+    # Make a directory in the file system
     if not os.path.exists(directory):
         os.makedirs(directory)
 
 
 def generate_pdf(filepath):
 
-    canvas_obj = canvas.Canvas(filepath,
-                               pagesize=A4)
+    styles = getSampleStyleSheet()
+    styleN = styles['Normal']
+    styleH = styles['Heading1']
+
+    def header(canvas, doc, content):
+        canvas.saveState()
+        w, h = content.wrap(doc.width, doc.topMargin)
+        content.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
+        canvas.restoreState()
+
+    doc = BaseDocTemplate(filepath, pagesize=A4)
+    frame = Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height - 2 * cm, id='normal')
+    header_content = Paragraph("This is a multi-line header.  It goes on every page.  " * 8, styleN)
+    template = PageTemplate(id='test', frames=frame, onPage=partial(header, content=header_content))
+    doc.addPageTemplates([template])
+
+    text = []
+    for i in range(111):
+        text.append(Paragraph("This is line %d." % i, styleN))
+
+    # Finish pdf
+    doc.build(text)
+
+
+    """
+    canvas_obj = canvas.Canvas(filepath, pagesize=A4)
 
     # Create textobject
     textobject = canvas_obj.beginText()
@@ -37,6 +65,7 @@ def generate_pdf(filepath):
 
     canvas_obj.drawText(textobject)
     canvas_obj.save()
+    """
 
 
 def open_pdf(filepath):
