af895a1cb9dfc0d9a68184b975bb80182af51cd9
Fasten90 <fasten90@gmail.com>
2021-11-01 23:13:13 +0100

    Unittest: move method to that file

--- PdfGenerator.py
+++ PdfGenerator.py
@@ -132,29 +132,6 @@ def generate_content():
     return content
 
 
-def generate_test_content():
-    content = []
-    styles = getSampleStyleSheet()
-    styleN = styles['Normal']
-
-    # Paragraph
-    for i in range(111):
-        content.append(Paragraph("This is line %d." % i, styleN))
-
-    # Table
-    data = [['00', '01', '02', '03', '04'],
-            ['10', '11', '12', '13', '14'],
-            ['20', '21', '22', '23', '24'],
-            ['30', '31', '32', '33', '34']]
-    t = Table(data)
-    # Set header style of table
-    t.setStyle(TableStyle([('BACKGROUND', (0, 0), (5, 0), colors.grey),
-                           ('TEXTCOLOR', (0, 0), (5, 0), colors.white)]))
-    content.append(t)
-
-    return content
-
-
 def open_pdf(file_path):
     os.system("start " + file_path)
 
---
+++ tests/test_pdf_generation.py
@@ -0,0 +1,35 @@
+import unittest
+
+import PdfGenerator
+
+
+def generate_test_content():
+    content = []
+    styles = getSampleStyleSheet()
+    styleN = styles['Normal']
+
+    # Paragraph
+    for i in range(111):
+        content.append(Paragraph("This is line %d." % i, styleN))
+
+    # Table
+    data = [['00', '01', '02', '03', '04'],
+            ['10', '11', '12', '13', '14'],
+            ['20', '21', '22', '23', '24'],
+            ['30', '31', '32', '33', '34']]
+    t = Table(data)
+    # Set header style of table
+    t.setStyle(TableStyle([('BACKGROUND', (0, 0), (5, 0), colors.grey),
+                           ('TEXTCOLOR', (0, 0), (5, 0), colors.white)]))
+    content.append(t)
+
+    return content
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
